b28ae84fddb979669e00e7e8a88715be846b40c5
yugao <gao.yu1@northeastern.edu>
2023-09-21 18:02:31 -0700

    Updated the picture

--- components/Carousel/index.js
+++ components/Carousel/index.js
@@ -7,17 +7,17 @@ function Carousel() {
             url: 'https://i.ibb.co/pRzdDxv/QQ-20230920170310.jpg',
         },
         {
-            url: 'https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80',
+            url: 'https://i.ibb.co/Fs8Bvvj/64bb82c5a5d656e5955d872678b33f4a.jpg',
         },
         {
-            url: 'https://images.unsplash.com/photo-1661961112951-f2bfd1f253ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2672&q=80',
+            url: 'https://i.ibb.co/D1Dy0kf/01d513413b93162f4178faab99a3f00b.jpg',
         },
 
         {
-            url: 'https://images.unsplash.com/photo-1512756290469-ec264b7fbf87?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2253&q=80',
+            url: 'https://i.ibb.co/JsWq4Tc/4fae5d1fb0a7b375ab970b4a94240dfa.jpg',
         },
         {
-            url: 'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2671&q=80',
+            url: 'https://i.ibb.co/wSHbnzx/f96859d3dc41325866db3ce320261a80.jpg',
         },
     ];
 
@@ -40,17 +40,19 @@ function Carousel() {
     };
 
     return (
-        <div className='max-w-[1400px] h-[500px] w-full m-auto py-16 px-4 relative group'>
-            <div
-                style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
-                className='w-full h-full rounded-2xl bg-center bg-cover duration-500'
-            ></div>
-            <button type="button" class="arrow flex absolute top-0 left-0 z-30 justify-center items-center px-4 h-full cursor-pointer group focus:outline-none" data-carousel-prev>
-                <svg onClick={prevSlide} class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="white" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path></svg>
-            </button>
-            <button type="button" class="arrow flex absolute top-0 right-0 z-30 justify-center items-center px-4 h-full cursor-pointer group focus:outline-none" data-carousel-next>
-                <svg onClick={nextSlide} stroke="white" class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path></svg>
-            </button>
+        <div>
+            <div className='max-w-[1400px] h-[500px] w-full m-auto py-16 px-4 relative group'>
+                <div
+                    style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
+                    className='w-full h-full rounded-xl bg-center bg-cover duration-500'
+                ></div>
+                <button type="button" className="arrow flex absolute top-0 left-0 z-30 justify-center items-center px-4 h-full cursor-pointer group focus:outline-none" data-carousel-prev>
+                    <svg onClick={prevSlide} className="w-8 h-8 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="white" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 19l-7-7 7-7"></path></svg>
+                </button>
+                <button type="button" className="arrow flex absolute top-0 right-0 z-30 justify-center items-center px-4 h-full cursor-pointer group focus:outline-none" data-carousel-next>
+                    <svg onClick={nextSlide} stroke="white" className="w-8 h-8 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7"></path></svg>
+                </button>
+            </div>
         </div>
     );
 }
